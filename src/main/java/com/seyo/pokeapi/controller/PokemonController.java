9b1c77b5d3751ab06c2ceaef5bf4fff3a1a22da6
Sandra Eyo <seyo.smr@gmail.com>
2024-10-21 19:55:39 +0200

    Eliminar error palabra git

--- src/main/java/com/seyo/pokeapi/controller/PokemonController.java
+++ src/main/java/com/seyo/pokeapi/controller/PokemonController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
-git
 import java.util.List;
 import java.util.Map;
 
